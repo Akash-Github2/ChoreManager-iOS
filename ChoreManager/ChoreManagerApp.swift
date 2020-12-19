9222002a7bacbd016e01ae6800c1479b47d7c8a3
Akash-Github2 <54249509+Akash-Github2@users.noreply.github.com>
2020-12-18 21:32:25 -0800

    Initial Commit

---
+++ ChoreManager/ChoreManagerApp.swift
@@ -0,0 +1,20 @@
+//
+//  ChoreManagerApp.swift
+//  ChoreManager
+//
+//  Created by Akash on 12/18/20.
+//
+
+import SwiftUI
+
+@main
+struct ChoreManagerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
